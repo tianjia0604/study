f0f23b99ace9d7306d8a0339355138e5f6571cef
tianjia0604 <1365751272@qq.com>
2020-04-27 22:21:11 +0800

    左旋转字符串

---
+++ leetcode/LeftRotateString.java
@@ -0,0 +1,28 @@
+package package4_27;
+
+/**
+ * @Author：Tina
+ * @Date：2020/4/27 22:18
+ */
+/**
+ * 左旋转字符串
+ * 字符串的左旋转操作是把字符串前面的若干个字符转移到字符串的尾部。请定义一个函数实现字符串左旋转操作的功能。比如，输入字符串"abcdefg"和数字2，该函数将返回左旋转两位得到的结果"cdefgab"。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入: s = "abcdefg", k = 2
+ * 输出: "cdefgab"
+ * 示例 2：
+ *
+ * 输入: s = "lrloseumgh", k = 6
+ * 输出: "umghlrlose"
+ */
+public class LeftRotateString {
+    public String reverseLeftWords(String s, int n) {
+        String str1 = s.substring(n);
+        String str2 = s.substring(0,n);
+        return str1 + str2;
+    }
+}
