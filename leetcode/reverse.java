296fd56006535aaa292641d732f8ad2256ea5a2e
tianjia0604 <1365751272@qq.com>
2020-05-04 22:52:12 +0800

    整数反转

---
+++ leetcode/reverse.java
@@ -0,0 +1,30 @@
+package leetcode6;
+
+/**7. 整数反转
+ 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+
+ 示例 1:
+
+ 输入: 123
+ 输出: 321
+ 示例 2:
+
+ 输入: -123
+ 输出: -321
+ 示例 3:
+
+ 输入: 120
+ 输出: 21
+ 注意:
+
+ 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−231,  231 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。*/
+public class reverse {
+    public int reverse(int x) {
+        long reverse = 0;
+        while( x != 0) {
+            reverse = reverse * 10 + x % 10;
+            x = x / 10;
+        }
+        return (int)reverse == reverse ? (int)reverse : 0;
+    }
+}
