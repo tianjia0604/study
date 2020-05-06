11e93933ebeb8dcd689b8ce5dbe0319e7c847396
tianjia0604 <1365751272@qq.com>
2020-05-06 22:17:10 +0800

    有效括号

---
+++ leetcode/isValid.java
@@ -0,0 +1,58 @@
+package leetcode6;
+
+import java.util.Stack;
+
+/**
+ * 20. 有效的括号
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ *
+ * 有效字符串需满足：
+ *
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ *
+ * 示例 1:
+ *
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ *
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ *
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ *
+ * 输入: "{[]}"
+ * 输出: true*/
+
+public class isValid {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack();
+        for(int i = 0;i < s.length();i++) {
+            char c = s.charAt(i);
+            if(c == '(' || c == '[' ||c == '{') {
+                stack.push(c);
+            }else {
+                if(stack.isEmpty()) {
+                    return false;
+                }
+                if(c == ')' && stack.pop() != '(') {
+                    return false;
+                }else if(c == ']' && stack.pop() != '[') {
+                    return false;
+                }else if(c == '}' && stack.pop() != '{') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
