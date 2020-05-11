718406ef2ce2de98125deeeb97f32ae94221257c
tianjia0604 <1365751272@qq.com>
2020-05-11 22:47:11 +0800

    x 的平方根

---
+++ leetcode/mySqrt.java
@@ -0,0 +1,36 @@
+package leetcode7;
+/**
+ * 69. x 的平方根
+ * 实现 int sqrt(int x) 函数。
+ *
+ * 计算并返回 x 的平方根，其中 x 是非负整数。
+ *
+ * 由于返回类型是整数，结果只保留整数的部分，小数部分将被舍去。
+ *
+ * 示例 1:
+ *
+ * 输入: 4
+ * 输出: 2
+ * 示例 2:
+ *
+ * 输入: 8
+ * 输出: 2
+ * 说明: 8 的平方根是 2.82842...,
+ *      由于返回类型是整数，小数部分将被舍去。*/
+public class mySqrt {
+    public int mySqrt(int x) {
+        int left = 0;
+        int right = x;
+        int ans = -1;
+        while(left <= right) {
+            int mid = left + (right - left) / 2;
+            if((long)mid * mid <= x) {
+                ans = mid;
+                left = mid + 1;
+            }else {
+                right = mid - 1;
+            }
+        }
+        return ans;
+    }
+}
