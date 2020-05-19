daf35a1c6cd6cb2647115364c922beb9fba198ed
tianjia0604 <1365751272@qq.com>
2020-05-19 23:02:29 +0800

    验证回文串

---
+++ leetcode/isPalindrome2.java
@@ -0,0 +1,31 @@
+package leetcode5;
+/**
+ * 125. 验证回文串
+ * 给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+ *
+ * 说明：本题中，我们将空字符串定义为有效的回文串。
+ *
+ * 示例 1:
+ *
+ * 输入: "A man, a plan, a canal: Panama"
+ * 输出: true
+ * 示例 2:
+ *
+ * 输入: "race a car"
+ * 输出: false*/
+public class isPalindrome2 {
+    public boolean isPalindrome(String s) {
+        if(s == null) {
+            return true;
+        }
+        s = s.toLowerCase();
+        int l = s.length();
+        StringBuilder str = new StringBuilder(l);
+        for(char c : s.toCharArray()) {
+            if((c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
+                str.append(c);
+            }
+        }
+        return str.toString().equals(str.reverse().toString());
+    }
+}
