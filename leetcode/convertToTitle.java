c44ed61f191e725c87c7befab73a0c3ffb2d0a6c
tianjia0604 <1365751272@qq.com>
2020-05-21 23:29:28 +0800

    Excel表列名称

---
+++ leetcode/convertToTitle.java
@@ -0,0 +1,38 @@
+package leetcode5;
+/**
+ * 168. Excel表列名称
+ * 给定一个正整数，返回它在 Excel 表中相对应的列名称。
+ *
+ * 例如，
+ *
+ *     1 -> A
+ *     2 -> B
+ *     3 -> C
+ *     ...
+ *     26 -> Z
+ *     27 -> AA
+ *     28 -> AB
+ *     ...
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: "A"
+ * 示例 2:
+ *
+ * 输入: 28
+ * 输出: "AB"
+ * 示例 3:
+ *
+ * 输入: 701
+ * 输出: "ZY"*/
+public class convertToTitle {
+    public String convertToTitle(int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        while (n != 0) {
+            n = n - 1;
+            stringBuilder.append((char)(n % 26 + 'A'));
+            n /= 26;
+        }
+        return stringBuilder.reverse().toString();
+    }
+}
