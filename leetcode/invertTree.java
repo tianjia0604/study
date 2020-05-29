2fd19988827f7539feb08fee75ae8a855ac7f23a
tianjia0604 <1365751272@qq.com>
2020-05-29 10:34:20 +0800

    翻转二叉树

---
+++ leetcode/invertTree.java
@@ -0,0 +1,38 @@
+package leetcode4tree;
+/**
+ * 226. 翻转二叉树
+ * 翻转一棵二叉树。
+ *
+ * 示例：
+ *
+ * 输入：
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * 输出：
+ *
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1*/
+public class invertTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode invertTree(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        TreeNode rightNode = root.right;
+        root.right = invertTree(root.left);
+        root.left = invertTree(rightNode);
+        return root;
+    }
+}
