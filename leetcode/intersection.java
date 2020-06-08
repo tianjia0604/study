d1537009604f8e262e03a92806503e094bf3d2d9
tianjia0604 <1365751272@qq.com>
2020-06-08 22:57:18 +0800

    两个数组的交集

---
+++ leetcode/intersection.java
@@ -0,0 +1,39 @@
+package leetcode8;
+
+import java.util.HashSet;
+import java.util.Set;
+/**
+ * 349. 两个数组的交集
+ * 给定两个数组，编写一个函数来计算它们的交集。
+ *
+ * 示例 1:
+ *
+ * 输入: nums1 = [1,2,2,1], nums2 = [2,2]
+ * 输出: [2]
+ * 示例 2:
+ *
+ * 输入: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * 输出: [9,4]*/
+public class intersection {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0) {
+            return new int[0];
+        }
+        Set<Integer> parentSet = new HashSet<>();
+        Set<Integer> childSet = new HashSet<>();
+        for (int num : nums1) {
+            parentSet.add(num);
+        }
+        for (int num : nums2) {
+            if (parentSet.contains(num)) {
+                childSet.add(num);
+            }
+        }
+        int[] res = new int[childSet.size()];
+        int index = 0;
+        for (int value : childSet) {
+            res[index++] = value;
+        }
+        return res;
+    }
+}
