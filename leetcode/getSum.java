dbf8a0a8b03bff4b46320f08589e23b29d3c0fa8
tianjia0604 <1365751272@qq.com>
2020-06-09 23:05:07 +0800

    两整数之和

---
+++ leetcode/getSum.java
@@ -0,0 +1,23 @@
+package leetcode7;
+/**
+ * 371. 两整数之和
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ *
+ * 示例 1:
+ *
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: a = -2, b = 3
+ * 输出: 1*/
+public class getSum {
+    public int getSum(int a, int b) {
+        while(b != 0){
+            int temp = a ^ b;
+            b = (a & b) << 1;
+            a = temp;
+        }
+        return a;
+    }
+}
