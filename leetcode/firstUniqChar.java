a5ad1b0e110f8a707783e380f60df61e38aba92d
tianjia0604 <1365751272@qq.com>
2020-06-11 23:40:32 +0800

    字符串中的第一个唯一字符

---
+++ leetcode/firstUniqChar.java
@@ -0,0 +1,32 @@
+package leetcode8;
+
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * 387. 字符串中的第一个唯一字符
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ *
+ *
+ * 示例：
+ *
+ * s = "leetcode"
+ * 返回 0
+ *
+ * s = "loveleetcode"
+ * 返回 2*/
+public class firstUniqChar {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>(26);
+        char[] chars = s.toCharArray();
+        for (char ch : chars) {
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (map.get(chars[i]) == 1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
